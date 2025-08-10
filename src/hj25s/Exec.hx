49c5adbbb1d2b9cc580c7aaa641dcde7462e7aaf
artem pecheny <pecheny@gmail.com>
2025-08-10 18:19:33 +0300

    More cards / actions.

--- src/hj25s/Exec.hx
+++ src/hj25s/Exec.hx
@@ -24,20 +24,50 @@ class ExecCtx extends Component {
     }
 
     public function enlarge(len) {}
-
-    public function addFork() {}
-
-    public function addFrag(len:Float) {
+    
+    public function incAbsorb(val:Float) {
         if (selection.value < 0)
             return;
         var papa = state.frags[selection.value];
-        var papaDir = papa.end - papa.pos;
+        papa.gathering.speed += val;
+        papa.onChange.dispatch();
+    }
+
+    public function addFork() {
+        if (selection.value < 0)
+            return;
+        var papa = state.frags[selection.value];
+        var dev = 1/4;
+        var len = 3 + Math.random() * 8;
+        var frag = createFrag(papa.end, (papa.end - papa.pos).rotate(  Math.PI * dev).angle, len);
+        regFrag(frag, papa);
+        len = 3 + Math.random() * 8;
+        frag = createFrag(papa.end, (papa.end - papa.pos).rotate( - Math.PI * dev).angle, len);
+        regFrag(frag, papa);
+    }
+
+    public function addFrag(len:Float, dev:Float = 1 / 4) {
+        if (selection.value < 0)
+            return;
+        var papa = state.frags[selection.value];
+        var frag = createFrag(papa.end, (papa.end - papa.pos).rotate((Math.random() - 0.5) * Math.PI * dev).angle, len);
+        regFrag(frag, papa);
+    }
+
+    function createFrag(pos, dir, len) {
         var frag = new RootFragment();
         frag.end.set(0, len);
-        frag.end.angle = papaDir.angle;
-        frag.end.rotate((Math.random() - 0.5) * Math.PI / 4);
-        frag.end += papa.end;
-        frag.pos += papa.end;
+        frag.end.angle = dir;
+        frag.end += pos;
+        frag.pos += pos;
+        return frag;
+    }
+
+    function regFrag(frag:RootFragment, ?papa:RootFragment) {
+        frag.parent = state.frags.indexOf(papa);
+        if (papa!=null) {
+            papa.children.push(state.frags.length);
+        }
         state.frags.push(frag);
         state.fragCreated.dispatch();
     }
